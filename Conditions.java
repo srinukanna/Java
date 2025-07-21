ecf13dad1146847374eae8f68b69eb10d3811bf9
srinukanna <kannasrinivasulu8@gmail.com>
2025-07-21 13:36:22 +0530

    myjava

--- Conditions.java
+++ Conditions.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 public class Conditions {
     public static void main(String[] args){
         
         final int x=10;
-
-        if(x>10){
+        try (Scanner sc = new Scanner(System.in)) {
+            int a =sc.nextInt();
+             int b = sc.nextInt();
+             int c=a+b;
+             System.out.println(c);
+        }
+          if(x>10){
             System.out.println("failed");
         }
         else{
