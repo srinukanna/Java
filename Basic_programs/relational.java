060afce754bdb211293881615ae5b978f44db7a0
srinukanna <kannasrinivasulu8@gmail.com>
2025-07-22 11:11:41 +0530

    myjava

---
+++ Basic_programs/relational.java
@@ -0,0 +1,16 @@
+package Basic_programs;
+
+public class relational {
+    public static void main(String[] args)
+    {
+      int a=100,b=120;
+      System.out.println(a>b);
+      System.out.println(a>=b);
+      System.out.println(a<b);
+      System.out.println(a<=b);
+      System.out.println(a==b);
+      System.out.println(a!=b);
+
+    }
+    
+}
